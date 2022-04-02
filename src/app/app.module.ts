d88c01786e166bf18faef9c51aab80e3502773de
Fenix Sampson <Fenix.sampson@gmail.com>
2022-04-02 19:31:28 -0400

    added base structure to welcome component and created about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { SelfieComponent } from './selfie/selfie.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     SelfieComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
