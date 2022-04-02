f17c08552e8b2e694363db6db105e0b2addfbdc3
Fenix Sampson <Fenix.sampson@gmail.com>
2022-04-02 18:36:44 -0400

    added nav info to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  pageTitle : string = 'Huston "Fenix" Sampson';
   title = 'angular-portfolio';
 }
