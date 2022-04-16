084b1f6e0f380f32639d07df9731604c16f89f3e
Fenix Sampson <Fenix.sampson@gmail.com>
2022-04-15 20:43:34 -0400

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname + "/dist/<fenix-sampson-portfolio-2022>"));
+app.use(express.static(__dirname + "/dist/fenix-sampson-portfolio-2022"));
 
 app.get("/*", function (req, res) {
   res.sendFile(
-    path.join(__dirname + "/dist/<fenix-sampson-portfolio-2022>/index.html")
+    path.join(__dirname + "/dist/fenix-sampson-portfolio-2022/index.html")
   );
 });
 
